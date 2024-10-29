d0d0d2735e2478d74519f1d8f4523868f25ad937
Kinlo Ephriam Tangiri (KET) <kinlotangiri911@gmail.com>
2024-10-29 21:08:33 +0100

    setup database and clerk sign in

---
+++ src/app/page.tsx
@@ -0,0 +1,51 @@
+"use client";
+import {
+  Authenticated,
+  Unauthenticated,
+  useMutation,
+  useQuery,
+} from "convex/react";
+import { useState } from "react";
+import { api } from "../../convex/_generated/api";
+import { SignInButton } from "@clerk/nextjs";
+
+export default function Home() {
+  const messages = useQuery(api.functions.message.list);
+  const createMessage = useMutation(api.functions.message.create);
+  const [input, setInput] = useState("");
+
+  console.log("Messages:", messages); // Debug: Check messages loaded
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("Submitting Message:", input); // Debug: Check input value on submit
+    createMessage({ sender: "Alice", content: input });
+    setInput("");
+  };
+
+  return (
+    <>
+      <Authenticated>
+        <div>
+          {messages?.map((message, index) => (
+            <div key={index}>
+              <strong>{message.sender}</strong>: {message.content}
+            </div>
+          ))}
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              placeholder="Type your message here..."
+            />
+            <button type="submit">Send</button>
+          </form>
+        </div>
+      </Authenticated>
+      <Unauthenticated>
+        <SignInButton />
+      </Unauthenticated>
+    </>
+  );
+}
